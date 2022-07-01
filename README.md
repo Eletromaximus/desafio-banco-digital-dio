df924567d2cc0a201810bc96ca4a97f34142e88c
Max Milliano <maxmillianox@protonmail.com>
2022-07-01 18:31:14 -0300

    conceito aplicado de herança, polimorfismo e abstração

---
+++ README.md
@@ -0,0 +1,3 @@
+## Desafio Dio Banco Digital Orientado a Objetos
+
+Nesse desafio, exercitamos os conceitos de herança, encapsulamento, abstração e polimorfismo.
---
+++ src/ContaCorrente.java
@@ -0,0 +1,41 @@
+public class ContaCorrente {
+  private int agencia;
+  private int numero;
+  private double saldo;
+
+  public void sacar() {
+
+  }
+
+  public int getAgencia() {
+    return agencia;
+  }
+
+  public void setAgencia(int agencia) {
+    this.agencia = agencia;
+  }
+
+  public int getNumero() {
+    return numero;
+  }
+
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  public double getSaldo() {
+    return saldo;
+  }
+
+  public void setSaldo(double saldo) {
+    this.saldo = saldo;
+  }
+
+  public void depositar() {
+    
+  }
+
+  public void transferir() {
+    
+  }
+}
